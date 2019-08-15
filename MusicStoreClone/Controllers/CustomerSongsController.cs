4b8cd59739fdaff022e6980a4638bbb7ce02f303
Jeff Cadorath <JJcadorath@hotmail.com>
2019-08-15 15:13:06 -0500

    Add project files.

---
+++ MusicStoreClone/Controllers/CustomerSongsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MusicStoreClone;
+
+namespace MusicStoreClone.Controllers
+{
+    public class CustomerSongsController : Controller
+    {
+        private ITunesDBEntities db = new ITunesDBEntities();
+
+        // GET: CustomerSongs
+        public ActionResult Index()
+        {
+            var customerSongs = db.CustomerSongs.Include(c => c.Customer).Include(c => c.Song);
+            return View(customerSongs.ToList());
+        }
+
+        // GET: CustomerSongs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerSong customerSong = db.CustomerSongs.Find(id);
+            if (customerSong == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerSong);
+        }
+
+        // GET: CustomerSongs/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "UserName");
+            ViewBag.SongId = new SelectList(db.Songs, "Id", "Title");
+            return View();
+        }
+
+        // POST: CustomerSongs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,SongId,CustomerId")] CustomerSong customerSong)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CustomerSongs.Add(customerSong);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "UserName", customerSong.CustomerId);
+            ViewBag.SongId = new SelectList(db.Songs, "Id", "Title", customerSong.SongId);
+            return View(customerSong);
+        }
+
+        // GET: CustomerSongs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerSong customerSong = db.CustomerSongs.Find(id);
+            if (customerSong == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "UserName", customerSong.CustomerId);
+            ViewBag.SongId = new SelectList(db.Songs, "Id", "Title", customerSong.SongId);
+            return View(customerSong);
+        }
+
+        // POST: CustomerSongs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,SongId,CustomerId")] CustomerSong customerSong)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customerSong).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "UserName", customerSong.CustomerId);
+            ViewBag.SongId = new SelectList(db.Songs, "Id", "Title", customerSong.SongId);
+            return View(customerSong);
+        }
+
+        // GET: CustomerSongs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerSong customerSong = db.CustomerSongs.Find(id);
+            if (customerSong == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerSong);
+        }
+
+        // POST: CustomerSongs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CustomerSong customerSong = db.CustomerSongs.Find(id);
+            db.CustomerSongs.Remove(customerSong);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MusicStoreClone/Controllers/CustomersController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MusicStoreClone;
+
+namespace MusicStoreClone.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        private ITunesDBEntities db = new ITunesDBEntities();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(db.Customers.ToList());
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UserName,Birthday")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserName,Birthday")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult SeeCustomerSongs()
+        {
+            var customers = db.Customers.ToList();
+            Logger.Info("Customer Searched For a specified Customer");
+            ViewBag.CustomerId = new SelectList(customers, "Id", "Id");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SeeCustomerSongs(int? CustomerId)
+        {
+            try
+            {
+                var result = db.Database.SqlQuery<SeeCustomersSongs>("EXEC SeeCustomerSong @CustomerId").ToList();
+                new SqlParameter("CustomerId", CustomerId);
+                Logger.Info("Customer Searched For their song list");
+                return View(result);
+            }
+            catch
+            {
+                Logger.Error("The CustomerID you looked for was not valid");
+                throw new Exception();
+            }
+        }
+
+        public ActionResult SeeSongsCustomers(int? songId = 2)
+        {
+            var result = db.Database.SqlQuery<SeeSongs>("EXEC SongsBoughtByCustomer @SongId").ToList();
+            new SqlParameter("SongId", songId);
+            Logger.Info("Customer Searched to see what songs");
+            return View(result);
+        }
+
+
+        public class SeeSongs
+        {
+            public string Song { get; set; }
+            public string Customers { get; set; }
+        }
+        public class SeeCustomersSongs
+        {
+            public string Song { get; set; }
+            public string Username { get; set; }
+        }
+    }
+}
